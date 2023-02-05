e1e4af8d76484065cb4711dcc198d4a5b4ef7994
harukasu4523 <airgir23@gmail.com>
2023-02-05 10:40:52 +0900

    整数１つをコンパイルするコンパイラを作成

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# bash -xで実行すると変数を展開して表示する
+
+assert(){
+  expected="$1"
+  input="$2"
+
+  ./9cc "$input" > tmp.s
+  cc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected, but got $actual"
+    exit 1
+  fi 
+}
+
+assert 0 0
+assert 42 42
+
+
+echo OK
